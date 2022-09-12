1f6054a00c174807eca2cb06b9eccd333104017d
Rolando L <rolando1515@hotmail.com>
2022-09-12 09:40:50 -0400

    Add files via upload

---
+++ Year_Make_Model autopopulate google sheet.js
@@ -0,0 +1,66 @@
+// These functions will call the NHTSA API and Auto-populate the cells on Google sheets
+// To run it, take out the comments, copy and paste the code on App scripps.
+// Then manually insert the function.
+// Example   =vinModelInfo(F2) 
+//           =vinMakeInfo(F2)
+//           =vinModelYearInfo(F2)   ------> f2 is the cell where you insert the VIN 
+// Drag and drop until desired cell
+
+function vinMakeInfo(vinNumber)
+{
+  if ((vinNumber) && (vinNumber != ""))
+  {
+    try
+    {
+      vin = encodeURI(vinNumber)
+      var url = "https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVinValues/"+vin+"?format=json"
+      var response = UrlFetchApp.fetch(url);
+      var w = JSON.parse(response.getContentText());
+      return w.Results[0].Make;
+    }
+    catch (e)
+    {      
+    }
+  }
+  return ""
+}
+
+
+function vinModelInfo(vinNumber)
+{
+  if ((vinNumber) && (vinNumber != ""))
+  {
+    try
+    {
+      vin = encodeURI(vinNumber)
+      var url = "https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVinValues/"+vin+"?format=json"
+      var response = UrlFetchApp.fetch(url);
+      var w = JSON.parse(response.getContentText());
+      return w.Results[0].Model;
+    }
+    catch (e)
+    {      
+    }
+  }
+  return ""
+}
+
+
+function vinModelYearInfo(vinNumber)
+{
+  if ((vinNumber) && (vinNumber != ""))
+  {
+    try
+    {
+      vin = encodeURI(vinNumber)
+      var url = "https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVinValues/"+vin+"?format=json"
+      var response = UrlFetchApp.fetch(url);
+      var w = JSON.parse(response.getContentText());
+      return w.Results[0].ModelYear;
+    }
+    catch (e)
+    {      
+    }
+  }
+  return ""
+}

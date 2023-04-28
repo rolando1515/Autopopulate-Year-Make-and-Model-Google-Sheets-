4c77df7d1ad98f31d196b41fd66fd72c85aad086
Rolando L <rolando1515@hotmail.com>
2023-04-27 20:53:36 -0400

    Update Year_Make_Model autopopulate google sheet.js

--- Year_Make_Model autopopulate google sheet.js
+++ Year_Make_Model autopopulate google sheet.js
@@ -6,6 +6,11 @@
 //           =vinModelYearInfo(F2)   ------> f2 is the cell where you insert the VIN 
 // Drag and drop until desired cell
 
+
+
+var REFRESH_INTERVAL_MINUTES = 2;
+
+
 function vinMakeInfo(vinNumber)
 {
   if ((vinNumber) && (vinNumber != ""))
@@ -63,4 +68,65 @@ function vinModelYearInfo(vinNumber)
     }
   }
   return ""
+}
+
+
+function vinAuctionInfo(vinNumber)
+{
+  if ((vinNumber) && (vinNumber != ""))
+  {
+    try
+    {
+      vin = encodeURI(vinNumber)
+      var url = "https://pinpoint-us-api.cox2m.com/v1/association?siteId=lmaa&size=1000&q=" + vin
+      var response = UrlFetchApp.fetch(url);
+      var w = JSON.parse(response.getContentText());
+      var total = w.total
+      
+      try
+      {
+        total = parseInt(total);
+      }
+      catch (e)
+      {
+        return "Invalid count in API result";
+      }
+      
+      if(total != 0)
+      {
+        return 'At Auction';
+      }
+      else
+      {
+        return 'Waiting Arrival';
+      }
+    }
+    catch (e)
+    {
+      return "Look VIN Manually";
+    }
+  }
+  return ""
+}
+
+
+function refresh() {
+  SpreadsheetApp.getUi().alert("Refreshing auction states");
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+}
+
+
+function onOpen() {
+  try {
+    SpreadsheetApp.getUi().alert("Testing periodic update");
+    ScriptApp.newTrigger('refresh')
+        .timeBased()
+        .everyMinutes(REFRESH_INTERVAL_MINUTES)
+        .create();
+    SpreadsheetApp.getUi().alert("Addred refresh trigger for ", REFRESH_INTERVAL_MINUTES.toString() + " Minutes");
+  }
+  catch (e)
+  {
+    SpreadsheetApp.getUi().alert(e.toString());
+  }
 }
